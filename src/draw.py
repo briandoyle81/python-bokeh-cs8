080148a118f1ccc72dda7292a6d60c7bba8a8cc2
briandoyle81 <briandoyle81@gmail.com>
2018-06-21 10:45:57 -0600

    edges drawing correctly

--- src/draw.py
+++ src/draw.py
@@ -5,26 +5,50 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
 
-N = 8
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
+
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
+
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+
+
+#this is drawing the edges from start to end
+start_indexes = []
+end_indexes = []
+
+for start_index, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indexes.append(start_index)
+        end_indexes.append(graph_data.vertexes.index(e.destination))
 
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=start_indexes, # a list of vertex indexes to start edges from
+    end=end_indexes) # a list of vertex indexes to end edges at
+
+print(graph.edge_renderer.data_source.data)
 
 ### start of layout code
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+# Looks like this is setting the positions of the vertexes
+
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
